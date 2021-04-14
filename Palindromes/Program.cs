359fdbd2d565c0abddf9f2472626d89ef8a70ab1
Shanen Cross <shanencross@yahoo.com>
2021-04-13 18:44:06 -0700

    Add game loop

--- Palindromes/Program.cs
+++ Palindromes/Program.cs
@@ -5,21 +5,33 @@ namespace Palindromes {
   public class Program {
     public static void Main() 
     {
+      bool isGameRunning = true;
       Console.WriteLine("Palindromes!");
       Console.WriteLine("They're words that look the same, whether you read them backwards or forwards!");
-      Console.WriteLine("Enter a word, and we'll tell you whether it's a palindrome or not: ");
+        
+      while (isGameRunning) { 
+        Console.WriteLine("Enter a word, and we'll tell you whether it's a palindrome or not: ");
 
-      string userInput = Console.ReadLine();
+        string userInput = Console.ReadLine();
 
-      bool isPalindrome = Palindrome.IsPalindrome(userInput);
+        bool isPalindrome = Palindrome.IsPalindrome(userInput);
 
-      if (isPalindrome) 
-      {
-        Console.WriteLine("Yep, it's a palindrome all right!");
-      }
-      else
-      {
-        Console.WriteLine("Nope, not a palindrome. How boring.");
+        if (isPalindrome) 
+        {
+          Console.WriteLine("Yep, it's a palindrome all right!");
+        }
+        else
+        {
+          Console.WriteLine("Nope, not a palindrome. How boring.");
+        }
+
+        Console.WriteLine("Would you like to go again? (y/n)");
+        string continueInput = Console.ReadLine();
+        Console.WriteLine();
+        if (continueInput != "y")
+        {
+          isGameRunning = false;
+        }
       }
     }
   }
